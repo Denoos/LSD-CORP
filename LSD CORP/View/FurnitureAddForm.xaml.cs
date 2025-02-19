a9a720db74187c9895a5b4f27ed7b3734007c588
Denoos <denoosdenoos@gmail.com>
2025-02-19 17:56:20 +1000

    Furniture Started

--- LSD CORP/View/FurnitureAddForm.xaml.cs
+++ LSD CORP/View/FurnitureAddForm.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,11 +19,32 @@ namespace LSD_CORP.View
     /// <summary>
     /// Логика взаимодействия для FurnitureAddForm.xaml
     /// </summary>
-    public partial class FurnitureAddForm : Window
+    public partial class FurnitureAddForm : Window, INotifyPropertyChanged
     {
+        private Furniture furniture;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void Signal([CallerMemberName] string? prop = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+
+        public Furniture Furniture { get => furniture; set { furniture = value; Signal(); } }
         public FurnitureAddForm()
         {
             InitializeComponent();
+            Furniture = new();
+            DataContext = this;
+        }
+
+        private async void SaveClick(object sender, RoutedEventArgs e)
+        {
+            if (await DataBase.Instance.AddFur(Furniture))
+                BackClick(sender, e);
+        }
+
+        private void BackClick(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            Close();
         }
     }
 }
